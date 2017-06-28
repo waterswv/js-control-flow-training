403e95003fd91cb20b75e95dbeb597be2b6ed16c
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-06-28 16:56:18 -0700

    added security question & answer prompts

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,36 @@
 console.log("security_questions.js loaded");
+
+let securityQuestions = [
+{
+  question: "what is your favorite movie?",
+  expectedAnswer: "Star Wars"
+},
+{
+  question: "what was your first car?",
+  expectedAnswer: "Honda Accord"
+},
+{
+  question: "what is your first name?",
+  expectedAnswer: "Bryan"
+}
+]
+
+
+console.log(securityQuestions);
+let userAnswer = "";
+
+for(i = 0; i <= securityQuestions.length - 1; i++){
+
+    userAnswer = prompt(securityQuestions[i].question);
+    if(userAnswer !== securityQuestions[i].expectedAnswer) {
+      console.log(userAnswer + " was User Answer #" + (i+1));
+      console.log(securityQuestions[i].expectedAnswer + " was expected for User Answer #" + (i+1));
+      alert("Sorry you're answer does not match");
+      break;
+    }
+    else {
+      console.log(userAnswer + " is User Answer #" + (i+1));
+    }
+}
+
+// "what is your favorite movie?":"Star Wars", "what was your first car":"Honda Accord", "what is your first name":"Bryan"
